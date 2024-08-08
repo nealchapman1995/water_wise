220101c44f48685705a35d0cac094e07d87e399a
nealchapman1995 <61425224+nealchapman1995@users.noreply.github.com>
2024-08-08 10:58:18 -0600

    First commit

---
+++ projects/my-firebase-app/src/configuration.jsx
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";  // Import other services as needed
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+console.log(process.env.REACT_APP_FIREBASE_API_KEY)
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services as needed
+const analytics = getAnalytics(app);
+const database = getDatabase(app);  // Initialize Realtime Database
+
+// Optionally, export the initialized Firebase app if you need it elsewhere
+export { app, database };
