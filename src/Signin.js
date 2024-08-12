1aa45a8f8057052affd4d55664936bee0a34f254
nealchapman1995 <61425224+nealchapman1995@users.noreply.github.com>
2024-08-11 18:03:33 -0600

    added some more css and link to signup page

--- src/Signin.js
+++ src/Signin.js
@@ -39,6 +39,11 @@ const SignInForm = ({ setUser }) => {
                 <button type="submit" className="flex w-full justify-center rounded-md bg-sky-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">Sign In</button>
             </form>
         </div>
+        <p className="mt-10 text-center text-sm text-gray-500">
+            <a href="/signup" className="font-semibold leading-6 text-sky-600 hover:text-sky-500">
+              Make an Account Now!
+            </a>
+          </p>
         {error && <p style={{ color: "red" }}>{error}</p>}
     </div>
   );
--- src/Signup.js
+++ src/Signup.js
@@ -34,23 +34,25 @@ const SignUpForm = ({ setUser }) => {
     };
 
   return (
-    <div>
-        <h2>Sign Up Here!</h2>
-        <form onSubmit={handleSignUp}>
-            <div>
-                <label>Email:</label>
-                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required></input>
-            </div>
-            <div>
-                <label>Password:</label>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required></input>
-            </div>
-            <div>
-                <label>Display Name</label>
-                <input type="text" value={name} onChange={(e) => setName(e.target.value)} required></input>
-            </div>
-            <button type="submit">Sign Up</button>
-        </form>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign Up Here!</h2>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form onSubmit={handleSignUp} className="space-y-6">
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <label className="block text-sm font-medium leading-6 text-gray-900">Email:</label>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"></input>
+                </div>
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <label className="block text-sm font-medium leading-6 text-gray-900">Password:</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"></input>
+                </div>
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <label className="block text-sm font-medium leading-6 text-gray-900">Display Name</label>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sku-600 sm:text-sm sm:leading-6"></input>
+                </div>
+                <button type="submit" className="flex w-full justify-center rounded-md bg-sky-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">Sign Up</button>
+            </form>
+        </div>
         {error && <p style={{ color: "red" }}>{error}</p>}
     </div>
   );
